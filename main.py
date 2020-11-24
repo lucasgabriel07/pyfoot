7d33668db2541773dde9f7bb0a5c3a664430e526
lucasgabriel07 <luck_as10@hotmail.com>
2020-11-24 00:39:23 -0300

    .

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from perguntas_e_respostas import perguntas, respostas
 from random import randint, shuffle
 from time import sleep
-from perguntas_e_respostas import perguntas, respostas
-
-
-def mensagem(string):
-    print('\n' + string)
 
 
 def ajuda():
-    comandos = """>> ajuda
+    comandos = """
+>> ajuda
 Exibe novamente os comandos.
 
 >> j
@@ -23,46 +20,48 @@ Vai para o próximo jogo."""
     return comandos
 
 
-def rodada(rodada):
+def iniciar_rodada(rodada):
     for i, jogo in enumerate(lista_de_jogos[rodada]):
         time1, time2 = jogo[0], jogo[1]
 
         if time_player == time1:
             sleep(0.5)
-            g1, g2 = partida(time2)  # Gols
+            gols1, gols2 = partida(time2)  # Gols
         elif time_player == time2:
             sleep(0.5)
-            g2, g1 = partida(time1)  # Gols
+            gols2, gols1 = partida(time1)  # Gols
         else:
-            g1 = randint(0, 4)  # Gols
-            g2 = randint(0, 4)  # Gols
+            gols1 = randint(0, 4)  # Gols
+            gols2 = randint(0, 4)  # Gols
 
-        lista_de_jogos[rodada][i][2] = g1
-        lista_de_jogos[rodada][i][3] = g2
-
-    mensagem(60 * '-')
+        lista_de_jogos[rodada][i][2] = gols1
+        lista_de_jogos[rodada][i][3] = gols2
 
 
 def partida(adversario):
     gols_player = 0
     gols_adversario = 0
 
-    mensagem(f'Começa o jogo! {time_player} x {adversario} pela {rodada_atual+1}ª Rodada.')
+    print(f'\nComeça o jogo! {time_player} x {adversario} pela {rodada_atual+1}ª Rodada.')
 
     for i in range(4):
         sleep(0.5)
-        time, g1, g2 = pergunta(adversario)
-        gols_player += g1
-        gols_adversario += g2
+        time = pergunta(adversario)
+        if time == time_player:
+            gols_player += 1
+        else:
+            gols_adversario += 1
         sleep(0.5)
-        mensagem(f'Gooooool do {time}!')
+        print(f'\nGooooool do {time}!')
+
         if index % 4 != 0 or index == 0:  # Não exibir na pergunta final de cada jogo
             sleep(1)
-            mensagem(f'Placar atual: {time_player} {gols_player} x {gols_adversario} {adversario}')
-            mensagem(60 * '-')
+            print(f'\nPlacar atual: {time_player} {gols_player} x {gols_adversario} {adversario}')
+            print(60 * '-')
 
     sleep(1)
-    mensagem(f'Fim de jogo! Placar Final: {time_player} {gols_player} x {gols_adversario} {adversario}')
+    print(f'\nFim de jogo! Placar Final: {time_player} {gols_player} x {gols_adversario} {adversario}')
+    print('\n' + 60 * '-')
 
     return gols_player, gols_adversario
 
@@ -73,23 +72,23 @@ def pergunta(adversario):
     pergunta = perguntas[index]
     resposta = respostas[index]
 
-    mensagem(pergunta)
+    print(pergunta)
     entrada = input('\nO que será exibido na tela? >> ')
 
     index += 1
 
     if entrada == resposta:
-        return time_player, 1, 0
-    return adversario, 0, 1
+        return time_player
+    return adversario
 
 
 def gerar_jogos():
     lista_auxiliar = lista_de_times[:]
     shuffle(lista_auxiliar)
     jogos = []
-    for rodada in range(7):  # Número de rodadas
+    for rodada in range(len(lista_de_times) - 1):  # Número de rodadas
         jogos_na_rodada = []
-        for i in range(4):  # Jogos por rodada
+        for i in range(len(lista_de_times)//2):  # Jogos por rodada
             time1 = lista_auxiliar[i]
             time2 = lista_auxiliar[-i-1]
             jogos_na_rodada.append([time1, time2, '', ''])
@@ -102,31 +101,31 @@ def gerar_jogos():
 
 
 def atualizar_pontuacao(rodada):
-    for i in range(8):  # Número de times
+    for i in range(len(lista_de_times)):  # Número de times
         time = lista_de_times[i]
-        for j in range(4):  # Jogos por rodada
-            time1, time2, g1, g2 = lista_de_jogos[rodada][j]
+        for j in range(len(lista_de_times)//2):  # Jogos por rodada
+            time1, time2, gols1, gols2 = lista_de_jogos[rodada][j]
             if time == time1:
-                if g1 > g2:
+                if gols1 > gols2:
                     pontuacao[i][0] += 3
-                elif g1 == g2:
+                elif gols1 == gols2:
                     pontuacao[i][0] += 1
-                pontuacao[i][1] += g1
-                pontuacao[i][2] += g2
-                pontuacao[i][3] += g1 - g2
+                pontuacao[i][1] += gols1
+                pontuacao[i][2] += gols2
+                pontuacao[i][3] += gols1 - gols2
             elif time == time2:
-                if g2 > g1:
+                if gols2 > gols1:
                     pontuacao[i][0] += 3
-                elif g1 == g2:
+                elif gols1 == gols2:
                     pontuacao[i][0] += 1
-                pontuacao[i][1] += g2
-                pontuacao[i][2] += g1
-                pontuacao[i][3] += g2 - g1
+                pontuacao[i][1] += gols2
+                pontuacao[i][2] += gols1
+                pontuacao[i][3] += gols2 - gols1
 
 
 def atualizar_classificacao():
-    for i in range(8):  # Número de times
-        for j in range(i+1, 8):  # Número de times
+    for i in range(len(lista_de_times)):  # Número de times
+        for j in range(i+1, len(lista_de_times)):  # Número de times
             if pontuacao[i][0] < pontuacao[j][0]:  # Pontos
                 lista_de_times[i], lista_de_times[j] = lista_de_times[j],  lista_de_times[i]
                 pontuacao[i], pontuacao[j] = pontuacao[j], pontuacao[i]
@@ -144,12 +143,12 @@ def tabela_de_jogos():
     print()
     print(60*'-')
     print('TABELA DE JOGOS'.center(60))
-    for rodada in range(7):  # Número de rodadas
+    for rodada in range(len(lista_de_jogos)):  # Número de rodadas
         print(60*'-')
         print(f'{rodada+1}ª RODADA'.center(60))
         print(f'{60*"-"}'.center(60))
 
-        for i in range(4):  # Jogos por rodada
+        for i in range(len(lista_de_times)//2):  # Jogos por rodada
             time1, time2, gols_time1, gols_time2 = lista_de_jogos[rodada][i]
             print(f'{time1:^20} {gols_time1:>1} x {gols_time2:<1} {time2:^20}'.center(60))
 
@@ -165,31 +164,32 @@ def classificacao():
     print(60 * '-')
     print(f"{'#':^3} {'TIMES':^36} {'PG':^4} {'GP':^4} {'GC':^4} {'SG':^4}".center(60))
     print(60*'-')
-    for i in range(8):  # Número de times
+    for i in range(len(lista_de_times)):  # Número de times
         time = lista_de_times[i]
         pg, gp, gc, sg = pontuacao[i]
         print(f'{i+1:^3} {time:<36}  {pg:^3}  {gp:^3}  {gc:^3}  {sg:^3}'.center(60))
 
 
-mensagem("""Você é um treinador de futebol iniciante e recebeu propostas
+print("""
+Você é um treinador de futebol iniciante e recebeu propostas
 dos 8 times do Campeonato Maranhense de 2020, e precisa 
 escolher qual time deseja comandar.
 
-    0) Cordino
-    1) Imperatriz
-    2) Juventude
-    3) Maranhão
-    4) Moto Club
-    5) Pinheiro
-    6) Sampaio Corrêa
-    7) São José""")
+    0) CORDINO
+    1) IMPERATRIZ
+    2) JUVENTUDE
+    3) MARANHÃO
+    4) MOTO CLUB
+    5) PINHEIRO
+    6) SAMPAIO CORRÊA
+    7) SÃO JOSÉ""")
 
-lista_de_times = ['Cordino', 'Imperatriz','Juventude', 'Maranhão',
-                  'Moto Club', 'Pinheiro', 'Sampaio Corrêa', 'São José']
+lista_de_times = ['CORDINO', 'IMPERATRIZ', 'JUVENTUDE', 'MARANHÃO',
+                  'MOTO CLUB', 'PINHEIRO', 'SAMPAIO CORRÊA', 'SÃO JOSÉ']
 
 lista_de_jogos = gerar_jogos()
 pontuacao = []
-for i in range(8):  # Número de times
+for i in range(len(lista_de_times)):  # Número de times
     pontuacao.append([0, 0, 0, 0])  # Pontos, Gols Feitos, Gols Sofridos, Saldo de Gols
 
 sleep(0.5)
@@ -200,18 +200,20 @@ while True:
         time_player = lista_de_times[index]
         break
     except (ValueError, IndexError):
-        mensagem('\033[31m' + 'Entrada inválida! Entre com um inteiro de 0 a 7.' + '\033[0;0m')
+        print('\033[1;31m \n' + 'Entrada inválida! Entre com um inteiro de 0 a 7.' + '\033[0;0m')
 
 sleep(0.5)
 
-mensagem(f"""Parabéns! Você é o novo treinador do {time_player}. 
+print(f"""
+Parabéns! Você é o novo treinador do {time_player}! 
 Esperamos que você honre nossa camisa e nos leve ao título!""")
 
 sleep(1.5)
 
-mensagem(24*'-' + ' INSTRUÇÕES ' + 24*'-')
+print('\n' + 24*'-' + ' INSTRUÇÕES ' + 24*'-')
 
-mensagem("""O jogo consiste em ler códigos em Python e responder qual será 
+print("""
+O jogo consiste em ler códigos em Python e responder qual será 
 a saída. Se você acertar, marcará um gol, mas se errar sofrerá 
 um gol. Serão respondidas 4 questões por jogo. O campeonato 
 será por pontos corridos, no sistema todos contra todos, e em 
@@ -219,41 +221,42 @@ turno único.""")
 
 sleep(0.5)
 
-mensagem("""Ao final de cada rodada, você pode optar por ver a 
+print("""
+Ao final de cada rodada, você pode optar por ver a 
 classificação, a tabela de jogos, ou ir para a próxima 
 rodada, usando os seguintes comandos:""")
 
 sleep(0.5)
 
-mensagem(ajuda())
+print(ajuda())
 
 sleep(0.5)
 
-mensagem(60*'-')
+print('\n' + 60*'-')
 
 rodada_atual = 0
 index = 0
 
-while rodada_atual <= 6:  # Número de rodadas - 1
+while rodada_atual <= len(lista_de_jogos) - 1:
 
     entrada = input('\nO que deseja fazer? >> ').lower()
 
     if entrada == 'ajuda':
-        mensagem(ajuda())
+        print(ajuda())
     elif entrada == 'c':
         classificacao()
     elif entrada == 'j':
         tabela_de_jogos()
     elif entrada == 'p':
-        rodada(rodada_atual)
+        iniciar_rodada(rodada_atual)
         atualizar_pontuacao(rodada_atual)
         atualizar_classificacao()
         rodada_atual += 1
     else:
-        mensagem('\033[31m' + 'Comando inválido!' + '\033[0;0m')
+        print('\033[1;31m \n' + 'Comando inválido!' + '\033[0;0m')
 
 sleep(1.5)
-mensagem(21*'-' + ' RESULTADO FINAL ' + 22*'-')
+print('\n' + 21*'-' + ' RESULTADO FINAL ' + 22*'-')
 sleep(1.5)
 tabela_de_jogos()
 sleep(1.5)
@@ -263,12 +266,13 @@ sleep(1.5)
 campeao = lista_de_times[0]
 
 if campeao == time_player:
-    mensagem(f'Parabéns! Seu time {time_player} foi campeão!')
+    print(f'\nParabéns! Seu time, {time_player}, sagrou-se campeão!')
 else:
     i = 0
-    while i <= 4:
+    while i <= 8:  # Número de times
         if lista_de_times[i] == time_player:
             break
         i += 1
 
-    mensagem(f'O {campeao} foi campeão. Seu time {time_player} ficou em {i+1}º lugar.')
+    print(f'\nO {campeao} foi campeão. Seu time {time_player} ficou na'
+          f' {i+1}º colocação.')
--- perguntas_e_respostas.py
+++ perguntas_e_respostas.py
@@ -1,4 +1,5 @@
-pergunta1 = """s = 'banana'
+pergunta1 = """
+s = 'banana'
 s = s.split('a')
 s1 = ''
 for i in s:
@@ -7,7 +8,8 @@ print(s1)"""
 
 resposta1 = 'bnn'
 
-pergunta2 = """v = [10, 3, 5, 4, 8]
+pergunta2 = """
+v = [10, 3, 5, 4, 8]
 for i in range(len(v)):
     for j in range(i+1, len(v)):
         if v[j] < v[i]:
@@ -16,12 +18,14 @@ print(v[2])"""
 
 resposta2 = '5'
 
-pergunta3 = """num = 177
+pergunta3 = """
+num = 177
 print(num % 2 == 0)"""
 
 resposta3 = 'False'
 
-pergunta4 = """v = [1, 2, 3, 4, 5]
+pergunta4 = """
+v = [1, 2, 3, 4, 5]
 soma = 0
 for n in v:
     if n % 2 == 0:
@@ -30,7 +34,8 @@ print(soma)"""
 
 resposta4 = '6'
 
-pergunta5 = """v = [3, 2, 1, 0]
+pergunta5 = """
+v = [3, 2, 1, 0]
 v2 = v[1:]
 soma = 0
 for i in v2:
@@ -39,17 +44,20 @@ print(soma)"""
 
 resposta5 = '3'
 
-pergunta6 = """s = 'UFMA'
+pergunta6 = """
+s = 'UFMA'
 print(s[-1])"""
 
 resposta6 = 'A'
 
-pergunta7 = """s = 'UFMA'
+pergunta7 = """
+s = 'UFMA'
 print(s[2:])"""
 
 resposta7 = 'MA'
 
-pergunta8 = """def func(num):
+pergunta8 = """
+def func(num):
     if num % 2 == 0 and num >= 0:
         return True
     return False
@@ -61,7 +69,8 @@ else:
 
 resposta8 = 'Não'
 
-pergunta9 = """def func(v):
+pergunta9 = """
+def func(v):
     return len(v) % 2 != 0
 
 v = ['A', 'B', 'C', 'D', 'E']    
@@ -69,17 +78,19 @@ print(func(v))"""
 
 resposta9 = 'True'
 
-pergunta10 = """v = []
+pergunta10 = """
+v = []
 i = 1
 while i <= 10:
     if i % 2 == 0:
-         v.append(i)
+        v.append(i)
     i += 1
 print(v[3])"""
 
 resposta10 = '8'
 
-pergunta11 = """v = []
+pergunta11 = """
+v = []
 for i in range(10):
     for j in range(10):
         v.append(i+j)
@@ -90,7 +101,8 @@ else:
 
 resposta11 = 'Não'
 
-pergunta12 = """s = 'UFMA'
+pergunta12 = """
+s = 'UFMA'
 for i in range(1, 11):
     if i % 3 == 0:
         s += str(i)
@@ -100,7 +112,8 @@ print(s)"""
 
 resposta12 = 'UFMA366911'
 
-pergunta13 = """v = [1, 3, 5, 7, 9]
+pergunta13 = """
+v = [1, 3, 5, 7, 9]
 v2 = []
 for i in range(1, len(v)+1):
     v2.append(v[-i])
@@ -108,7 +121,8 @@ print(v2[3])"""
 
 resposta13 = '3'
 
-pergunta14 = """s = 'UFMA'
+pergunta14 = """
+s = 'UFMA'
 s2 = ''
 for i in range(1, len(s)+1):
     s2 += s[-i]
@@ -116,7 +130,8 @@ print(s2)"""
 
 resposta14 = 'AMFU'
 
-pergunta15 = """n = '1'
+pergunta15 = """
+n = '1'
 n += '0'
 n = int(n)
 n = n//3
@@ -124,7 +139,8 @@ print(n)"""
 
 resposta15 = '3'
 
-pergunta16 = """a = 3
+pergunta16 = """
+a = 3
 b = '5'
 c = str(a) + b
 a += int(c)
@@ -132,41 +148,48 @@ print(a)"""
 
 resposta16 = '38'
 
-pergunta17 = """a = '1.5'
+pergunta17 = """
+a = '1.5'
 a = float(a)
 print(a*3)"""
 
 resposta17 = '4.5'
 
-pergunta18 = """s = '11'
+pergunta18 = """
+s = '11'
 print(s*2)"""
 
 resposta18 = '1111'
 
-pergunta19 = """a = '10'
+pergunta19 = """
+a = '10'
 b = 10
 print(a==b)"""
 
 resposta19 = 'False'
 
-pergunta20 = """a = 10.0
+pergunta20 = """
+a = 10.0
 b = 10
 print(a==b)"""
 
 resposta20 = 'True'
 
-pergunta21 = """v = [1, 2, 3, 4]
+pergunta21 = """
+v = [1, 2, 3, 4]
 v.append(v[3] + 1)
 print(v[-1])"""
 
 resposta21 = '5'
 
-pergunta22 = """v = [1, 2, 3, 4]
+pergunta22 = """
+v = [1, 2, 3, 4]
 print(v[-1] == v[3])"""
 
 resposta22 = 'True'
 
-pergunta23 = """s = ''
+pergunta23 = """
+s = ''
 for i in range(1, 10, 2):
     s += str(i)
 n = int(s) // 100
@@ -174,35 +197,40 @@ print(n)"""
 
 resposta23 = '135'
 
-pergunta24 = """v = []
+pergunta24 = """
+v = []
 for i in range(2, 20, 3):
     v.append(i)
 print(14 in v)"""
 
 resposta24 = 'True'
 
-pergunta25 = """v = []
+pergunta25 = """
+v = []
 for i in range(2, 20, 3):
     v.append(i)
 print(20 in v)"""
 
 resposta25 = 'False'
 
-pergunta26 = """def func(num):
+pergunta26 = """
+def func(num):
     return num ** 2
 
 print(func(12))"""
 
 resposta26 = '144'
 
-pergunta27 = """def func(num1, num2):
+pergunta27 = """
+def func(num1, num2):
     return num1 ** num2
 
 print(func(2, 3))"""
 
 resposta27 = '8'
 
-pergunta28 = """def func(n1, n2):
+pergunta28 = """
+def func(n1, n2):
     soma = 0
     for i in range(0, n1+1, n2):
         soma += i
